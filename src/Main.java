f6e2b374a31c5e09cc21c2e4129e3a31546c5984
paolo9688 <paolo.pz1@gmail.com>
2025-01-31 16:58:59 +0100

    feat: ho aggiornato il file Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("[38;5;208mAntipasti:");
+        System.out.println("[38;5;208m- Crostini con senape, prosciutto cotto e formaggio");
+        System.out.println("[38;5;208m- Quesadilla all'italiana con Asiago");
+        System.out.println("[38;5;208m- Omeletta di patate, Trentino-Alto Adige");
+        System.out.println("[38;5;208m- Mini canederli con sugo alla puttanesca");
+        System.out.println("[38;5;208m- Montanarine");
+        System.out.println("[38;5;208m- Gamberi al miele e pan di spezie abbrustolito");
     }
 }
