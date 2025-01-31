803cee493bbcd341f042b438852c2b3d6204b274
Gabriele Gaita <gabrielegaita001@gmail.com>
2025-01-31 17:58:29 +0100

    Aggiunta la stampa con il relativo colore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("\033[31mDessert\n"+
+                            "- Tiramisù classico\n"+
+                            "Mascarpone, uova, zucchero, savoiardi, caffè\n"+
+                            "- Millefoglie alle fragole\n"+
+                            "Pasta Sfoglia, Zucchero, Fragole, Panna fresca liquida, Zeccheri a velo, Estratto di vaniglia, zucchero a velo\n"+
+                            "- Babà al rum\n"+
+                            "Farina Manitoba, Burro, Zucchero, Uova, Lievito di birra fresco, sale fino\n"+
+                            "- Strudel di mele\n"+
+                            "Mele Golden, Zucchero, Pangrattato, Burro, Uvetta, Pinoli tostati, Cannella in polvere, Rum, Scorza di limone\n"+
+                            "- Cannoli siciliani\n"+
+                            "Ricotta di pecora,  Zucchero, Gocce di cioccolato fondente");
+
+
     }
 }
