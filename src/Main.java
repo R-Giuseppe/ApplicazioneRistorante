bdc2cf9c7bd5c7981e20c5541279d6130c4b58c3
Dennis Ciccone <dennisciccone02@gmail.com>
2025-02-01 17:00:05 +0100

    Aggiunta la stampa dei secondi piatti

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\u001B[35mSecondi piatti\n");
 
+        System.out.println("\u001B[35mPollo alla cacciatora");
+        System.out.println("\u001B[35m-Pollo cotto in un sugo di pomodoro, cipolla, aglio, olive nere, capperi, vino rosso, rosmarino.\n");
+
+        System.out.println("\u001B[35mArrosto di vitello");
+        System.out.println("\u001B[35m-Vitello cotto con rosmarino, salvia, vino bianco e brodo.\n");
+
+        System.out.println("\u001B[35mSpezzatino di manzo");
+        System.out.println("\u001B[35m-Manzo cotto in un sugo di pomodoro, cipolla, vino rosso, brodo, rosmarino e alloro.\n");
+
+        System.out.println("\u001B[35mScaloppine al limone");
+        System.out.println("\u001B[35m-Vitello in padella con burro, succo di limone, vino bianco e prezzemolo.\n");
+
+        System.out.println("\u001B[35mBistecca alla fiorentina");
+        System.out.println("\u001B[35m-Bistecca di manzo cotta sulla griglia, servita con olio d'oliva e pepe nero.\n");
     }
 }
