910a8245965980d90d1cb7ce081bd695288ae10a
scarf89 <phoenixserviceoff@gmail.com>
2025-02-05 16:07:47 +0100

    Ho aggiornato il file main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("*** Primi Piatti *** ");
+        System.out.println("Pappardelle");
+        System.out.println("Pasta l'uovo con sugo al Cinghiale");
+        System.out.println("Lasagna");
+        System.out.println("Pasta all'uovo con carne macinata, uovo e mozzarella");
+        System.out.println("Gnocchi");
+        System.out.println("Pasta all'uovo con sugo al Ragu'");
+        System.out.println("Paccheri");
+        System.out.println("Pasta secca con Tartufo e Funghi Porcini");
+        System.out.println("Spaghetti");
+        System.out.println("Pasta secca con condimento alla Carbonara");
+
+
+
     }
 }
