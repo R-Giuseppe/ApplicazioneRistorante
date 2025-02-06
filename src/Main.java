ce945a84396d90f405472f8e85e32301d970c9b5
paolo9688 <paolo.pz1@gmail.com>
2025-02-06 16:19:05 +0100

    feat: ho creato la stampa degli antipasti

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[38;5;208mAntipasti:");
-        System.out.println("[38;5;208m- Crostini con senape, prosciutto cotto e formaggio");
-        System.out.println("[38;5;208m- Quesadilla all'italiana con Asiago");
-        System.out.println("[38;5;208m- Omeletta di patate, Trentino-Alto Adige");
-        System.out.println("[38;5;208m- Mini canederli con sugo alla puttanesca");
-        System.out.println("[38;5;208m- Montanarine");
-        System.out.println("[38;5;208m- Gamberi al miele e pan di spezie abbrustolito");
+        System.out.println("\u001B[33m "+
+            "### Antipasti\n" +
+            "\n" +
+            "- Crostini\n" +
+            "Crostini con senape, prosciutto cotto e formaggio \n" + "\n"+
+            "- Quesadilla\n" +
+            "Quesadilla all'italiana con Asiago\n" + "\n"+
+            "- Omeletta\n" +
+            "Omeletta di patate, Trentino-Alto Adige\n" + "\n"+
+            "- Canederli\n" +
+            "Mini canederli con sugo alla puttanesca\n" + "\n"+
+            "- Montanarine\n" +
+            "Montanarine con burrata e salmone affumicato\n" + "\n"+
+            "- Gamberi\n" +
+            "Gamberi al miele e pan di spezie abbrustolito");
     }
 }
