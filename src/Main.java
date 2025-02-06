45bd957910c7cdc775c303a0e063ba7155b1a51a
Dennis Ciccone <dennisciccone02@gmail.com>
2025-02-06 17:11:54 +0100

    feat: aggiunta stampa secondi piatti

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\u001B[35mSecondi piatti\n");
-
-        System.out.println("\u001B[35mPollo alla cacciatora");
-        System.out.println("\u001B[35m-Pollo cotto in un sugo di pomodoro, cipolla, aglio, olive nere, capperi, vino rosso, rosmarino.\n");
-
-        System.out.println("\u001B[35mArrosto di vitello");
-        System.out.println("\u001B[35m-Vitello cotto con rosmarino, salvia, vino bianco e brodo.\n");
-
-        System.out.println("\u001B[35mSpezzatino di manzo");
-        System.out.println("\u001B[35m-Manzo cotto in un sugo di pomodoro, cipolla, vino rosso, brodo, rosmarino e alloro.\n");
-
-        System.out.println("\u001B[35mScaloppine al limone");
-        System.out.println("\u001B[35m-Vitello in padella con burro, succo di limone, vino bianco e prezzemolo.\n");
-
-        System.out.println("\u001B[35mBistecca alla fiorentina");
-        System.out.println("\u001B[35m-Bistecca di manzo cotta sulla griglia, servita con olio d'oliva e pepe nero.\n");
+        System.out.println("\033[35m "+
+                "### Secondi Piatti\n" +
+                "\n" +
+                "- Pollo alla cacciatora\n" +
+                "Pollo cotto in un sugo di pomodoro, cipolla, aglio, olive nere, capperi, vino rosso, rosmarino. \n" + "\n"+
+                "- Arrosto di vitello\n" +
+                "Vitello cotto con rosmarino, salvia, vino bianco e brodo.\n" + "\n"+
+                "- Spezzatino di manzo\n" +
+                "Manzo cotto in un sugo di pomodoro, cipolla, vino rosso, brodo, rosmarino e alloro.\n" + "\n"+
+                "- Scaloppine al limone\n" +
+                "Vitello in padella con burro, succo di limone, vino bianco e prezzemolo.\n" + "\n"+
+                "- Bistecca alla fiorentina\n" +
+                "Bistecca di manzo cotta sulla griglia, servita con olio d'oliva e pepe nero.\n");
     }
 }
