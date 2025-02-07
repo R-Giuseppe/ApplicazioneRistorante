0db0c28f3195857f0459bfa248b222716e7d90b5
Gabriele Gaita <gabrielegaita001@gmail.com>
2025-02-07 04:13:55 +0100

    Sistemata la stampa della sezione feature/Dessert

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("\033[31mDessert\n"+
+        System.out.println("\033[31mDessert\n"+ "\n" +
                             "- Tiramisù classico\n"+
-                            "Mascarpone, uova, zucchero, savoiardi, caffè\n"+
+                            "Mascarpone, uova, zucchero, savoiardi, caffè\n"+ "\n"+
                             "- Millefoglie alle fragole\n"+
-                            "Pasta Sfoglia, Zucchero, Fragole, Panna fresca liquida, Zeccheri a velo, Estratto di vaniglia, zucchero a velo\n"+
+                            "Pasta Sfoglia, Zucchero, Fragole, Panna fresca liquida, Zeccheri a velo, Estratto di vaniglia, zucchero a velo\n"+ "\n"+
                             "- Babà al rum\n"+
-                            "Farina Manitoba, Burro, Zucchero, Uova, Lievito di birra fresco, sale fino\n"+
+                            "Farina Manitoba, Burro, Zucchero, Uova, Lievito di birra fresco, sale fino\n"+ "\n"+
                             "- Strudel di mele\n"+
-                            "Mele Golden, Zucchero, Pangrattato, Burro, Uvetta, Pinoli tostati, Cannella in polvere, Rum, Scorza di limone\n"+
+                            "Mele Golden, Zucchero, Pangrattato, Burro, Uvetta, Pinoli tostati, Cannella in polvere, Rum, Scorza di limone\n"+ "\n" +
                             "- Cannoli siciliani\n"+
                             "Ricotta di pecora,  Zucchero, Gocce di cioccolato fondente");
 
