ba5f265434c6bbcad1f35365a0d3ef0750b7d4ac
Dennis Ciccone <dennisciccone02@gmail.com>
2025-02-19 16:26:53 +0100

    Aggiunta la classe astratta Portate e aggiornata la classe Secondi Piatti

--- src/SecondiPiatti.java
+++ src/SecondiPiatti.java
@@ -1,54 +1,16 @@
-public class SecondiPiatti {
+public class SecondiPiatti extends Portate {
 
     // Attributi
-    private String nome, descrizione;
-    private double prezzo;
-    private String[] ingredienti;
     private boolean tipoCottura; // true = ben cotto; false = al sangue
 
     // Costruttore
     public SecondiPiatti(String nome, String descrizione, double prezzo, String[] ingredienti, boolean tipoCottura) {
-        this.nome = nome;
-        this.descrizione = descrizione;
-        this.prezzo = prezzo;
-        this.ingredienti = ingredienti;
+        super(nome, descrizione, prezzo, ingredienti);
         this.tipoCottura = tipoCottura;
     }
 
 
     // Setter and Getter
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescrizione() {
-        return descrizione;
-    }
-
-    public void setDescrizione(String descrizione) {
-        this.descrizione = descrizione;
-    }
-
-    public double getPrezzo() {
-        return prezzo;
-    }
-
-    public void setPrezzo(double prezzo) {
-        this.prezzo = prezzo;
-    }
-
-    public String[] getIngredienti() {
-        return ingredienti;
-    }
-
-    public void setIngredienti(String[] ingredienti) {
-        this.ingredienti = ingredienti;
-    }
-
     public boolean isTipoCottura() {
         return tipoCottura;
     }
@@ -58,7 +20,7 @@ public class SecondiPiatti {
 
 
     //Funzione che restituisce una stringa contenente l'elenco degli ingredienti dell'istanza
-    private String elencoIngredienti() {
+    public String elencoIngredienti() {
         String list = "";
         for (int i = 0; i < ingredienti.length - 1; i++) {
             list += ingredienti[i] + ", ";
